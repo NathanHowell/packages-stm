2e8315d74c5563e40ec059de8e656634e95c2a01
Nathan Howell <nathan.d.howell@gmail.com>
2025-12-30 14:23:02 -0800

    Update STM wrappers for plan-only STM

--- Control/Monad/STM.hs
+++ Control/Monad/STM.hs
@@ -49,7 +49,7 @@ module Control.Monad.STM (
 
 #ifdef __GLASGOW_HASKELL__
 #if ! (MIN_VERSION_base(4,3,0))
-import GHC.Conc hiding (catchSTM)
+import GHC.Conc
 import Control.Monad    ( MonadPlus(..) )
 import Control.Exception
 #else
@@ -103,38 +103,6 @@ instance Alternative STM where
 -- @since 2.1.1
 check :: Bool -> STM ()
 check b = if b then return () else retry
-#endif
-
-#if ! (MIN_VERSION_base(4,3,0))
--- |Exception handling within STM actions.
-catchSTM :: Exception e => STM a -> (e -> STM a) -> STM a
-catchSTM (STM m) handler = STM $ catchSTM# m handler'
-    where
-      handler' e = case fromException e of
-                     Just e' -> case handler e' of STM m' -> m'
-                     Nothing -> raiseIO# e
-
--- | A variant of 'throw' that can only be used within the 'STM' monad.
---
--- Throwing an exception in @STM@ aborts the transaction and propagates the
--- exception. (Note: Allocation effects, such as  'newTVar' are not rolled back
--- when this happens. All other effects are discarded. See <https://gitlab.haskell.org/ghc/ghc/-/issues/18453 ghc#18453.>)
---
--- Although 'throwSTM' has a type that is an instance of the type of 'throw', the
--- two functions are subtly different:
---
--- > throw e    `seq` x  ===> throw e
--- > throwSTM e `seq` x  ===> x
---
--- The first example will cause the exception @e@ to be raised,
--- whereas the second one won\'t.  In fact, 'throwSTM' will only cause
--- an exception to be raised when it is used within the 'STM' monad.
--- The 'throwSTM' variant should be used in preference to 'throw' to
--- raise an exception within the 'STM' monad because it guarantees
--- ordering with respect to other 'STM' operations, whereas 'throw'
--- does not.
-throwSTM :: Exception e => e -> STM a
-throwSTM e = STM $ raiseIO# (toException e)
 #endif
 
 
